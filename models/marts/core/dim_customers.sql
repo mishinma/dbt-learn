cd5a69f2d31b10d7674acef6ce561630ea8550de
Mikhail Mishin <mikhail.mishin@oda.com>
2021-11-13 11:39:34 +0100

    Reorganize project

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,25 +1,18 @@
+
+
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
 
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
+),
+
+
+with orders as (
+
+    select * from {{ ref('stg_orders') }}
 
 ),
 
-orders as (
-
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
-
-),
 
 customer_orders as (
 
